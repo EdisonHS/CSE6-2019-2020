40386f2726e15f65841dab3d83f11e722d71b43a
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2019-11-01 13:33:21 -0700

    Added Math

--- notes.py
+++ notes.py
@@ -9,4 +9,50 @@ print(15)
 
 print('hello')
 # print("world')
-Print("things")
+# Print("things")
+
+
+# Data Types
+print("hello")  # This is a string
+print(13.543)  # This is a float
+print(False)  # This is a boolean
+print("seven")  # This is a string
+print(-81)  # This is a integer
+
+# This is a comment
+'''
+This is a multi-line comment
+I can type anything here
+
+'''
+
+"""
+I can also use quotation marks
+to indicate a multi-line comment
+"""
+
+# ----------
+# Math
+# ----------
+
+print()  # This creates a blank line
+print()
+print(3 + 5)
+print(8 - 7)
+print(4 * 3)
+print(20 / 5)
+
+print()
+print()
+print(2 ** 4)  # The result is 16
+print(3 ** 2)  # The result is 9
+print(4 ** 2)  # The result is 16
+print(2 ** 10)  # The result is 1024
+# The double star is the symbol for _______
+
+print()
+print()
+print(12 // 3)
+print(9 // 3)
+print(10 // 3)
+print(11 // 3)
