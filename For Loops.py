5a249fb25f2eba67ce05f8f6827ba283b312f9dc
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2020-01-28 13:56:24 -0800

    Added For loops

---
+++ For Loops.py
@@ -0,0 +1,52 @@
+for variable in range(10):
+    print("Hello World")
+
+for iterator in range(1500):
+    print('Computer Science')
+
+for i in range(2, 5):
+    print("How many times will this print?")
+
+for i in range(4, 10):
+    print("Magic")
+
+for i in range(8):
+    print(200)
+
+for i in range(5):
+    print(i)
+
+for i in range(12):
+    print(i)
+
+for i in range(5, 10):
+    print(i)
+
+for i in range(20, 25):
+    print(i + 4)
+
+for i in range(20, 25):
+    print("The value of 'i' is: ")
+    print(i)
+    print("But it prints this instead: ")
+    print(i + 4)
+    print()
+
+for i in range(0, 16, 2):
+    print(i)
+
+for i in range(1, 17, 2):
+    print(i)
+
+for i in range(1, 8, 2):
+    print(i)
+
+for i in range(10, 0, -1):
+    print("Time Left: {}".format(i))
+
+# ----------
+# For Loops Day 2
+# ----------
+print()
+print("Day 2 notes")
+print()
