49d36e1fcfdea49da25c17890f6268f743ad8fdd
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2020-01-29 13:59:45 -0800

    Finished For loops

--- For Loops.py
+++ For Loops.py
@@ -50,3 +50,28 @@ for i in range(10, 0, -1):
 print()
 print("Day 2 notes")
 print()
+# Adding the numbers 1 through 100
+# Could you imagine trying to do this by hand?
+# It is tedious. Annoying. Long.
+# For loops make it easy.
+total = 0
+for i in range(1, 101):  # How do I get 1 through 100?
+    total += i
+    print("The total so far is {}".format(total))
+print("The final total is {}".format(total))
+# You should have 5050.
+
+# Divisibility
+# We will attempt to print out ONLY numbers
+# divisible by 2 between 1 and 100.
+for i in range(1, 101):
+    if i % 2 == 0:  # If i is evenly divisible by 2
+        print(i)
+
+for i in range(1, 101):
+    if i % 3 == 0:  # If i is evenly divisible by 3
+        print(i)
+
+for i in range(1, 101):
+    if i % 2 == 0 and i % 3 == 0:
+        print(i)
